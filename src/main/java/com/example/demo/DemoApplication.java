12ed0b7679980b50c8ef74bdcd1050cd019012ea
orsaada <orsaa@post.bgu.ac.il>
2021-03-14 00:51:36 +0200

    change url path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,19 +11,11 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-//		return "Hello World";
-//		return List.of("Hello", "World");
-		return List.of(new Student(1L, "Mariam","mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5), 21));
-
-	}
 }
