fa08a8d9810ad274e987a87964387dd3572af270
orsaada <orsaa@post.bgu.ac.il>
2021-03-14 01:32:14 +0200

    open db and connect to it

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,17 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
     @GetMapping
 	public List<Student> getStudents(){
-//		return "Hello World";
-//		return List.of("Hello", "World");
-		return List.of(new Student(1L, "Mariam","mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5), 21));
+        return studentService.getStudents();
 
 	}
 }
